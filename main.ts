7d77cb8b2f95a0445d6ee9bb01bde28710c11a13
Fung YIP <fung.yip@gmail.com>
2023-08-13 10:37:04 +0800

    testing

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
 basic.showIcon(IconNames.Happy)
-music.changeTempoBy(20)
-music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Birthday), music.PlaybackMode.InBackground)
-music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Wedding), music.PlaybackMode.InBackground)
+music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
+music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Chase), music.PlaybackMode.InBackground)
+music.play(music.createSoundExpression(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+music.setBuiltInSpeakerEnabled(false)
 basic.forever(function () {
     if (TPBot.sonarJudge(TPBot.Sonarjudge.Less, 20)) {
         TPBot.setTravelTime(TPBot.DriveDirection.Forward, 60, 5)
