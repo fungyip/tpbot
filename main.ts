fe790596f4c6f4f57c1906ed503291ebe36036c7
Fung YIP <fung.yip@gmail.com>
2023-08-13 20:25:59 +0800

    upgrade the logic with senor

--- main.ts
+++ main.ts
@@ -5,8 +5,12 @@ music.play(music.createSoundExpression(WaveShape.Sine, 5000, 0, 255, 0, 500, Sou
 music.setBuiltInSpeakerEnabled(false)
 basic.forever(function () {
     if (TPBot.sonarJudge(TPBot.Sonarjudge.Less, 20)) {
-        TPBot.setTravelTime(TPBot.DriveDirection.Forward, 60, 5)
-        TPBot.setTravelTime(TPBot.DriveDirection.Right, 55, 2)
+        TPBot.setTravelTime(TPBot.DriveDirection.Backward, 81, 5)
         TPBot.headlightColor(0xff8000)
+        TPBot.setTravelTime(TPBot.DriveDirection.Right, 55, 2)
+        TPBot.headlightColor(0x00ff00)
+        if (TPBot.trackLine(TPBot.TrackingState.L_R_line)) {
+            TPBot.setTravelSpeed(TPBot.DriveDirection.Left, 100)
+        }
     }
 })
